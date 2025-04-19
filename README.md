8e009ad5ed22f6338d9166b760e7356ff0ede5ed
Yves Orton <yves.orton@remote.com>
2025-04-20 00:14:40 +0200

    ready to publish as 0.1.0

--- README.md
+++ README.md
@@ -12,14 +12,20 @@ with code like this:
 x = whatever when condition()
 ```
 
+For instance you can write
+
+```elixir
+x = 1 when x == nil
+```
+
 Exports no functions, just a macro. The macro does no validation,
 but it does work on tuples and anything else that can be expressed
 as THING = THING
 
 ## Installation
 
-If [available in Hex](https://hex.pm/docs/publish), the package can be installed
-by adding `assign_when` to your list of dependencies in `mix.exs`:
+The package can be installed by adding `assign_when` to your list of dependencies
+in `mix.exs`:
 
 ```elixir
 def deps do
@@ -29,7 +35,4 @@ def deps do
 end
 ```
 
-Documentation can be generated with [ExDoc](https://github.com/elixir-lang/ex_doc)
-and published on [HexDocs](https://hexdocs.pm). Once published, the docs can
-be found at <https://hexdocs.pm/assign_when>.
-
+The documentation for this package can be found at <https://hexdocs.pm/assign_when>.
--- mix.exs
+++ mix.exs
@@ -5,24 +5,50 @@ defmodule AssignWhen.MixProject do
     [
       app: :assign_when,
       version: "0.1.0",
-      elixir: "~> 1.15",
+      elixir: "~> 1.0",
+      build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      name: "AssignWhen",
+      description: description(),
+      package: package(),
+      deps: deps(),
+      source_url: "https://github.com/demerphq/assign_when"
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
+  defp description() do
+    """
+    Allows the use of `when` clauses on assignment statements as an alternative to using `if`,
+    avoiding the need for boilerplate `else` clauses which return the original variables when
+    the condition is `false`. Thus you can write
+
+    ```elixir
+    x = 1 when x == nil`
+    ```
+
+    instead of
+
+    ```elixir
+    x = if x == nil, do: 1, else: x`
+    ```
+    """
+  end
+
+  defp package do
+    [
+      name: :assign_when,
+      licenses: ["Apache-2.0"],
+      links: %{"GitHub" => "https://github.com/demerphq/assign_when"},
+      maintainers: ["Yves Orton"],
+      files: ~w(lib mix.exs .formatter.exs README.md LICENSE)
+    ]
+  end
+
   def application do
-    [
-      extra_applications: [:logger]
-    ]
+    []
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
-    [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-    ]
+    [{:ex_doc, "~> 0.14", only: :dev, runtime: false}]
   end
 end
